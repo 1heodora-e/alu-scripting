bcde585a4e73b96b907ba1992c314fac8cd6ec91
1heodora-e <t.egbunike@alustudent.com>
2024-07-06 12:16:18 +0000

    This is the final commit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Get the top ten hot posts of a subreddit"""
+"""get subscribers function"""
+
 
 import json
 import requests
 import sys
 
-def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit"""
+
+def number_of_subscribers(subreddit):
+    """get all subscribers"""
     if len(sys.argv) < 2:
-        print(None)
+        return 0
     else:
-        url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {"User-Agent": "Mozilla/5.0"}
         result = requests.get(url, headers=headers, allow_redirects=False)
         if result.status_code != 200:
-            print(None)
-            return
+            return 0
         body = json.loads(result.text)
-        posts = body.get("data", {}).get("children", [])
-        if not posts:
-            print(None)
-            return
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
-
+        return body["data"]["subscribers"]
